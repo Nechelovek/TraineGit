3afbac6f0b6ad82410e6b88012169d01382c3ed9
Nechelovek <ivan.batura.programmer@gmail.com>
2021-09-06 17:11:29 +0300

    Test

---
+++ M05_Logging/M05_Logging/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Parser;
+
+namespace M05_Logging
+{
+    class Program
+    {
+        private static ILogger logger = new NLogLoggerFactory().CreateLogger("Logger");
+  
+        static void Main(string[] args)
+        {
+            logger.LogDebug("Start program");
+            Parse.Logger = new NLogLoggerProvider().CreateLogger("Logger");
+            
+            var number = "123";
+            Console.WriteLine($"You entered: {number}");
+            
+            try
+            {
+                logger.LogDebug($"Try to convert entered string {nameof(number)} to int");
+                
+                Console.WriteLine($"Your number was converted to int result: {Parse.ToInt(number)}");
+            }
+            catch (FormatException fe)
+            {
+                logger.LogError(fe, fe.Message);
+                
+                Console.WriteLine($"You entered invalid data {number}, please check and fix it!");
+                
+                logger.LogDebug("The application failed, please see Errors.log");
+                return;
+            }
+            catch (ArgumentNullException ane)
+            {
+                logger.LogError(ane, ane.Message);
+                
+                Console.WriteLine($"You entered invalid data {number}, please check and fix it!");
+                
+                logger.LogDebug("The application failed, please see Errors.log");
+                return;
+            } 
+            
+            logger.LogDebug("Program is completed");
+        }
+    }
+}
---
+++ M05_Logging/Parser/Parse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Parser
+{
+    public class Parse
+    {
+        private const int AskiiValueForNumber = 48;
+
+        public static ILogger Logger { private get; set; }
+       
+        private static int CharToInt(char number)
+        {
+            Logger.LogDebug($"Try to convert symbol {number} to number");
+            if (number - AskiiValueForNumber < 0 || number - AskiiValueForNumber > 9)
+            {
+                throw new FormatException("Input argument is not correct");
+            }
+            
+            return number - AskiiValueForNumber;
+        }
+
+        public static int ToInt(string number)
+        {
+            _ = number ?? throw new ArgumentNullException(nameof(number), "Argument was null, it is not possible");
+            if (number == "")
+            {
+                throw new FormatException("Input argument is not correct");
+            }
+
+            var result = 0;
+            var length = number.Length - 1;
+            
+            Logger.LogDebug($"Start convert argument ({nameof(number)}:{number}) to int");
+            for (int i = 0; i < number.Length; i++)
+            {
+                try
+                {
+                    result += CharToInt(number[i]) * (int)Math.Pow(10, length--);
+                }
+                catch (FormatException)
+                {
+                    Logger.LogError($"{number[i]} impossible convert to number. Input string was {number}");
+                    throw;
+                }
+            }
+
+            return result;
+        }
+    }
+}
